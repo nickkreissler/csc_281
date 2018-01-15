d5f93459a7966d12a2e7818f637d65dcf184969f
nickkreissler <nick.d.kreissler@gmail.com>
2018-01-14 19:08:02 -0800

    update

--- CSC_281/src/homework1/DiceProb.java
+++ CSC_281/src/homework1/DiceProb.java
@@ -4,7 +4,6 @@ public class DiceProb {
 
 	public static void main(String[] args) {
 		int rounds = 1000;
-		int r = 5;
 		int total = 0;
 		int z = 0;
 		int x;
@@ -16,6 +15,7 @@ public class DiceProb {
 		// as needed until we've seen a sum of 5 precisely 1000 times
 		// The rolling of the dice will be doing using a call to rollDice
 		// Don't duplicate that code in the main
+		for (int r = 2; r < 13; r++) {
 		while (z < 1000) {
 			x = rollDice();
 			if (x == r) {
@@ -24,8 +24,9 @@ public class DiceProb {
 		total++;
 	}
 		System.out.println("It took " + total + " rounds to reach " + rounds + " rolls of " + r + ".");
-
-	}
+		z = 0;
+		total = 0;
+	}}
 	
 	public static int rollDice() {
 		int x =(int) (1+ (Math.random()*6));
